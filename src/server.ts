b5ce4ba9c7c1f714e532957c498067c601c8a7c2
vanagila <vanagilakedna@gmail.com>
2023-10-20 23:48:03 -0300

    add routes

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,17 @@
 import cors from "cors";
 import "dotenv/config";
 import express from "express";
+import { likeRoutes, tweetRoutes, userRoutes } from "./routes";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
+
+app.use("/user", userRoutes());
+app.use("/tweet", tweetRoutes());
+app.use("/like", likeRoutes());
 
 app.listen(process.env.PORT, () =>
   console.log(`Servidor rodando da porta ${process.env.PORT}`)
